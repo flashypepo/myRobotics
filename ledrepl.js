4094f1fb5f652c97270cbdd94ea84f2a1b7983cc
PePo <flexykukulkan@icloud.com>
2016-05-02 23:23:53 +0200

    Johnny-Five example (REPL)
    LED pin 3, REPL to control LED interactively.
    Demo for classroom.

--- ledrepl.js
+++ ledrepl.js
@@ -4,8 +4,12 @@ var board = new five.Board();
 board.on("ready", function() {
   console.log("Ready event. Repl instance auto-initialized!");
 
-  var led = new five.Led(9); // 2016_0112 led op 9
-
+  var led = new five.Led(3); // 2016_0112 led was op 9
+    //* use REPL
+    this.repl.inject({
+        l : led
+    });
+  /*/
   this.repl.inject({
     // Allow limited on/off control access to the
     // Led instance from the REPL.
@@ -16,4 +20,6 @@ board.on("ready", function() {
       led.off();
     }
   });
+  //*/
+  led.on();
 });
